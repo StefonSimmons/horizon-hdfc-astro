6c5bae5de910b6315b55051790875fb4976d264b
StefonSimmons <57869435+StefonSimmons@users.noreply.github.com>
2025-05-18 08:04:51 -0400

    init jwt

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,8 @@
 import { defineAction, ActionError } from 'astro:actions';
 import { z } from 'astro:schema';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
 
 export const server = {
     getUser: defineAction({
@@ -8,11 +10,10 @@ export const server = {
         input: z.object({
             passcode: z.string(),
         }),
-        handler: async ({ passcode }) => {
+        handler: async ({passcode}) => {
             // bcrypt.hash(passcode, saltRounds=10, function(err, hash) {
             //     console.log("DIGEST::",passcode,"--",hash)
             // });
-
             const hash = import.meta.env.SECRET_PROD_DIGEST || import.meta.env.SECRET_DEV_DIGEST || ""
             const result = await bcrypt.compare(passcode, hash)
 
@@ -24,10 +25,37 @@ export const server = {
             }
 
             // else return success
+            const token = jwt.sign({ user: "horizon_shareholder" }, import.meta.env.JWT_SECRET, {
+                expiresIn: '7d',
+            });
+            
             return {
                 code: "Authorized",
                 message: "Passcode is valid.",
+                token: token,
             }
         },
+    }),
+    verifyUser: defineAction({
+        accept: 'json',
+        input: z.object({
+            token: z.string(),
+        }),
+        handler: async ({token}) => {
+            // Verify the token
+            try {
+                const decoded = jwt.verify(token, import.meta.env.JWT_SECRET);
+                return {
+                    code: "Authorized",
+                    message: "Token is valid.",
+                    user: decoded,
+                }
+            } catch (err) {
+                throw new ActionError({
+                    code: "UNAUTHORIZED",
+                    message: "Invalid token.",
+                })
+            }
+        }
     })
 }
